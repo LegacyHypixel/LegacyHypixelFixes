d5c8e39bf6540e3b9887b2ea8b9b752697548a9d
RacoonDog <32882447+RacoonDog@users.noreply.github.com>
2023-08-05 01:07:29 -0400

    wowie that's already a bugfix

--- src/main/java/io/github/racoondog/legacyhypixelfixes/mixin/ScoreboardMixin.java
+++ src/main/java/io/github/racoondog/legacyhypixelfixes/mixin/ScoreboardMixin.java
@@ -3,6 +3,7 @@ package io.github.racoondog.legacyhypixelfixes.mixin;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.scoreboard.Scoreboard;
+import net.minecraft.scoreboard.ScoreboardObjective;
 import net.minecraft.scoreboard.Team;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,7 +14,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Scoreboard.class)
 public abstract class ScoreboardMixin {
     @Inject(method = "removeTeam", at = @At("HEAD"), cancellable = true)
-    private void preventLogSpam(Team team, CallbackInfo ci) {
+    private void preventLogSpam$0(Team team, CallbackInfo ci) {
         if (team == null) ci.cancel();
+    }
+
+    @Inject(method = "removeObjective", at = @At("HEAD"), cancellable = true)
+    private void preventLogSpam$1(ScoreboardObjective objective, CallbackInfo ci) {
+        if (objective == null) ci.cancel();
     }
 }
